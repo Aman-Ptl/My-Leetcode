fa37d063e83164c4a816ac38983502c2fa6e2ea7
Aman Patel <Ptlaman2711@gmail.com>
2025-08-20 22:22:22 +0530

    Time: 2 ms (12.68%), Space: 18.5 MB (78.75%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -24,7 +24,7 @@ public:
 
         // set marker in the first row/col
         for(int i=1; i<m; i++){
-            for(int j=1; j<n; j++){   // ✅ start from j=1
+            for(int j=1; j<n; j++){   // start from j=1
                 if(matrix[i][j]==0){
                     matrix[i][0]=0;
                     matrix[0][j]=0;
@@ -36,7 +36,7 @@ public:
         for(int i=1; i<m; i++){
             for(int j=1; j<n; j++){
                 if(matrix[i][0]==0 || matrix[0][j]==0){
-                    matrix[i][j] = 0;   // ✅ assignment fixed
+                    matrix[i][j] = 0;   //assignment fixed
                 }
             }
         }
